2ce0af3cb8346142d3bb634a815b4b701a66d45c
Guoliang HE <guolianghe1996@gmail.com>
2023-11-19 17:59:17 +0000

    add relay bench back

--- benchmark_scripts/relay_build.py
+++ benchmark_scripts/relay_build.py
@@ -25,7 +25,7 @@ import numpy as np
 
 import tvm
 from tvm import relay
-from tvm import relax
+from tvm.relay.backend.executor_factory import ExecutorFactoryModule
 from tvm import meta_schedule as ms
 from tvm.meta_schedule.testing.relay_workload import get_network
 from tvm.meta_schedule.testing.tune_utils import generate_input_data
@@ -33,7 +33,7 @@ from tvm.target.target import Target
 from tvm.runtime.vm import Executable
 from tvm.runtime.module import Module
 
-from tvm.relax.testing.relay_translator import from_relay
+import tvm.contrib.graph_executor as graph_executor
 
 # benchmark utils
 sys.path.append(os.path.dirname(sys.path[0]))
@@ -56,28 +56,37 @@ logging.basicConfig(
 logging.getLogger("tvm.meta_schedule").setLevel(logging.DEBUG)
 
 
-def ms_tune(mod, target, params):
-    mod = relax.transform.BindParams("main", params)(mod)
+def ms_tune(
+    mod: tvm.IRModule,
+    params,
+    input_shape: Tuple[int],
+    target: Target,
+):
     with tempfile.TemporaryDirectory() as work_dir:
-        db = ms.relax_integration.tune_relax(
-            mod=mod,
-            target=target,
-            params=params,
-            task_scheduler="round-robin",
-            num_trials_per_iter=32,
-            max_trials_per_task=32,
-            max_trials_global=1024,
-            work_dir=work_dir,
-        )
-        ex = ms.relax_integration.compile_relax(
-            db,
-            mod=mod,
-            target=target,
-            params=params,
-        )
-    dev = tvm.device(str(target), 0)
-    vm = relax.VirtualMachine(ex, dev)
-    return ex, vm
+        with ms.Profiler() as profiler:
+            database = ms.relay_integration.tune_relay(
+                mod=mod,
+                target=target,
+                params=params,
+                work_dir=work_dir,
+                max_trials_global=configs.num_measure_trials,
+            )
+            lib: ExecutorFactoryModule = ms.relay_integration.compile_relay(
+                database=database,
+                mod=mod,
+                target=target,
+                params=params,
+                backend='graph',
+            )
+
+        # must exit profiler scope
+        print(f'profiler:: ')
+        print(profiler.table())
+
+        # 'llvm' -> tvm.cpu(0)
+        device = tvm.device(str(target), 0)
+        graph_module = graph_executor.GraphModule(lib["default"](device))
+    return graph_module
 
 
 def build(
@@ -87,14 +96,14 @@ def build(
     target: Target,
 ):
     with tvm.transform.PassContext(opt_level=3):
-        ex = relax.build(
+        lib: ExecutorFactoryModule = relay.build(
             mod,
             target=target,
-            # params=params,  # this has problem?
+            params=params,
         )
         dev = tvm.device(str(target), 0)
-        vm = relax.VirtualMachine(ex, dev)
-    return ex, vm
+        graph_module = graph_executor.GraphModule(lib["default"](dev))
+    return graph_module
 
 
 def main(_):
@@ -108,7 +117,7 @@ def main(_):
     print(f"using target: {FLAGS.t}")
     target = configs.available_targets[FLAGS.t]
 
-    if bool(FLAGS.d):
+    if FLAGS.d:
         # so that python stops and we can attach debugger
         pid = os.getpid()
         input(f"attach to pid {pid}")
@@ -126,25 +135,18 @@ def main(_):
             dtype=dtype,
             workload_shape=shape)
 
-        # to relax (from test_relay_translator.py)
-        relax_mod = from_relay(
-            mod['main'],
-            relay_params=params,
-            target=target,
-            opt_level=-1,  # disable relay optimization
-        )
-
         # Compile the optimised tensor code for each tensor operator
+        print(f"Tuning and Compiling {model_name} with {FLAGS.mode}...")
         if FLAGS.mode == 'tune':
-            ex, vm = ms_tune(
-                mod=relax_mod,
+            graph_module = ms_tune(
+                mod=mod,
                 params=params,
                 input_shape=input_shape,
                 target=target,
             )
         elif FLAGS.mode == 'build':
-            ex, vm = build(
-                mod=relax_mod,
+            graph_module = build(
+                mod=mod,
                 params=params,
                 input_shape=input_shape,
                 target=target,
@@ -152,25 +154,23 @@ def main(_):
         else:
             raise RuntimeError(f"Unknown mode: {FLAGS.mode}")
 
-        # generate input
+        # inputs = generate_input_data(
+        #     input_shape=input_shape,
+        #     input_dtype=dtype,
+        #     low=0.,
+        #     high=1.,
+        # )
         dev = tvm.device(str(target), 0)
-        data = tvm.nd.array(
-            np.random.rand(*input_shape).astype(np.float32), dev)
+        result = graph_module.benchmark(dev)
 
-        # INFERENCE
-        # res = vm["main"](data)
-        # out = res.numpy()
-
-        # BENCHMARK
         # cost = result.mean * 1e3
-        res = vm.time_evaluator(
-            func_name='main',
-            dev=dev,
-            number=10,
-            repeat=1,
-        )(data)
-        print(f"result: ")
-        print(res)
+        print(f"results: ")
+        print(result)
+
+        # Export the library for future use; TODO linker fail in macbook!
+        # compiled_model = f"{model_name}_{compiled_suffix}.so"
+        # lib.export_library(str(operator_libs_path.joinpath(f"{compiled_model}")))
+        # print(f"Exported compiled library to {compiled_model}")
 
 
 if __name__ == "__main__":
